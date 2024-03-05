f3487b734400d1c6b7557a83b684d1c2be8405cc
Cigno05 <paolocalcagni.82@gmail.com>
2024-03-05 16:35:14 +0100

    Form Milestone 2 funzionante

--- js/app.js
+++ js/app.js
@@ -1,24 +1,28 @@
 console.log('JS Funziona!');
 /* input km da percorrere */
-const inputKmElement = document.getElementById('km');
+const inputKmElement = document.getElementById('km'); //element \ null
 console.log(inputKmElement);
 
 /*input età passeggero */
-const inputAgeElement = document.getElementById('age');
+const inputAgeElement = document.getElementById('age'); //element \ null
 console.log(inputAgeElement);
 
+/*input per sconto select*/
+const select18Element = document.getElementById('discount18'); //element \ null
+console.log(select18Element)
+
 /*invio dati*/
-const SubmitElement = document.getElementById('submit');
+const submitElement = document.getElementById('submit'); 
 
 /*listner*/
 let km, age;
 
-SubmitElement.addEventListener('click', function() {
+submitElement.addEventListener('click', function() {
     /*risultato input km*/
-    km = inputKmElement.value
+    km = parseFloat(inputKmElement.value); //number
 
     /*risultato input age*/
-    age = inputAgeElement.value
+    age = parseInt(inputAgeElement.value); //number
 
     /*stampa risultati*/
     console.log('Kilometri ' + km);
@@ -29,8 +33,8 @@ SubmitElement.addEventListener('click', function() {
     console.log('Costo Km dichiarati ' + priceForKm + ' €');
 
     /*risultato calcolo biglietto*/
-    const calcResult = document.getElementById('calc-result')
-    console.log(calcResult)
+    const calcResult = document.getElementById('calc-result');
+    console.log(calcResult);
 
     /*Validation*/
 
@@ -40,21 +44,44 @@ SubmitElement.addEventListener('click', function() {
         
         const discountUnder18 = (priceForKm - (priceForKm * 0.2)); //number
         const discountOver65 = (priceForKm - (priceForKm * 0.4)); //number
-    
-        /* calcolo prezzo del biglietto in base agli sconti*/
 
-        if (age < 18) {
+        /*
+        alternativa
+        const discountUnder18 = (priceForKm * 0.8); //number
+        const discountOver65 = (priceForKm * 0.6); //number
+        */
+    
+        /* calcolo prezzo del biglietto in base agli sconti milestone 1*/
+
+        // if (age < 18) {
+        //     let ticketPrice = discountUnder18.toFixed(2); //number
+        //     console.log(ticketPrice)
+        //     calcResult.innerHTML += ticketPrice
+        // } else if (age > 65) {
+        //     let ticketPrice = discountOver65.toFixed(2); //number
+        //     console.log(ticketPrice)
+        //     calcResult.innerHTML += ticketPrice
+        // } else { // 35
+        //     console.log(priceForKm.toFixed(2)); //number
+        //     calcResult.innerHTML += priceForKm
+        // }
+
+        /* calcolo prezzo del biglietto in base agli sconti del select (Milestone 2)*/
+
+        if (age == 2) {
             let ticketPrice = discountUnder18.toFixed(2); //number
             console.log(ticketPrice)
             calcResult.innerHTML += ticketPrice
-        } else if (age > 65) {
+        } else if (age == 3) {
             let ticketPrice = discountOver65.toFixed(2); //number
-            console.log(ticketPrice)
+            console.log(ticketPrice);
             calcResult.innerHTML += ticketPrice
         } else { // 35
             console.log(priceForKm.toFixed(2)); //number
             calcResult.innerHTML += priceForKm
         }
+
+
         
         
         
