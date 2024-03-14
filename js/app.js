5fc6345a969b68c30aa41eead84a1533e98dfcbd
Cigno05 <paolocalcagni.82@gmail.com>
2024-03-14 22:46:57 +0100

    Add: ticket

--- js/app.js
+++ js/app.js
@@ -1,32 +1,49 @@
 console.log('JS Funziona!');
+// input name da percorrere
+const inputNameElement = document.getElementById('name'); //element \ null
+
+//input surname da percorrere
+const inputSurnameElement = document.getElementById('surname'); //element \ null
+
+
 /* input km da percorrere */
 const inputKmElement = document.getElementById('km'); //element \ null
-console.log(inputKmElement);
+// console.log(inputKmElement);
 
 /*input età passeggero */
 const inputAgeElement = document.getElementById('age'); //element \ null
-console.log(inputAgeElement);
+// console.log(inputAgeElement);
 
 /*input per sconto select*/
 const select18Element = document.getElementById('discount18'); //element \ null
-console.log(select18Element)
+// console.log(select18Element)
 
 /*invio dati*/
-const submitElement = document.getElementById('submit'); 
+const submitElement = document.getElementById('submit');
 
 /*listner*/
 let km, age;
 
-submitElement.addEventListener('click', function() {
+submitElement.addEventListener('click', function () {
+    // risultato input name
+    userName = inputNameElement.value; // string
+
+    // risultato input Surname
+    userSurname = inputSurnameElement.value; //string
+
+    const userCompleteName = userName + ' ' + userSurname;
     /*risultato input km*/
     km = parseFloat(inputKmElement.value); //number
-
+    
     /*risultato input age*/
     age = parseInt(inputAgeElement.value); //number
-
+    
     /*stampa risultati*/
+    console.log(userName);
+    console.log(userSurname);
+    console.log(userCompleteName)
     console.log('Kilometri ' + km);
-    console.log('Età dichiarata ' + age + ' anni' );
+    console.log('Età dichiarata ' + age + ' anni');
 
     /*calcolo prezzo per km*/
     const priceForKm = km * 0.21; //string
@@ -34,15 +51,15 @@ submitElement.addEventListener('click', function() {
 
     /*risultato calcolo biglietto*/
     const calcResult = document.getElementById('calc-result');
-    
-    console.log(calcResult);
 
     /*Validation*/
 
     if (km > 0 && age > 0 && !isNaN(km) && !isNaN(age)) {
 
+
+
         /*sconti sul prezzo del biglietto*/
-        
+
         const discountUnder18 = (priceForKm - (priceForKm * 0.2)); //number
         const discountOver65 = (priceForKm - (priceForKm * 0.4)); //number
 
@@ -51,7 +68,7 @@ submitElement.addEventListener('click', function() {
         const discountUnder18 = (priceForKm * 0.8); //number
         const discountOver65 = (priceForKm * 0.6); //number
         */
-    
+
         /* calcolo prezzo del biglietto in base agli sconti milestone 1*/
 
         // if (age < 18) {
@@ -68,30 +85,53 @@ submitElement.addEventListener('click', function() {
         // }
 
         /* calcolo prezzo del biglietto in base agli sconti del select (Milestone 2)*/
+        let discountPrice; 
+        let ticketPrice;
 
         if (age == 2) {
-            let ticketPrice = discountUnder18.toFixed(2); //number
-            console.log(ticketPrice)
-            calcResult.innerHTML += ticketPrice
-        } else if (age == 3) {
-            let ticketPrice = discountOver65.toFixed(2); //number
+            ticketPrice = discountUnder18.toFixed(2); //number
             console.log(ticketPrice);
-            calcResult.innerHTML += ticketPrice
-        } else { // 35
+            calcResult.innerHTML += ('€ ' + ticketPrice);
+            discountPrice = 20;
+        } else if (age == 3) {
+            ticketPrice = discountOver65.toFixed(2); //number
+            console.log(ticketPrice);
+            calcResult.innerHTML += ('€ ' + ticketPrice);
+            discountPrice = 40;
+        } else { 
+            ticketPrice = priceForKm;
             console.log(priceForKm.toFixed(2)); //number
-            calcResult.innerHTML += priceForKm
+            calcResult.innerHTML += ('€ ' + ticketPrice);
+            discountPrice = 0;
         }
 
 
-        
-        
-        
-        
-        
-        
-        
-        
-        }
+        const ticketCard = document.getElementById('ticket-card');
+        console.log(ticketPrice);
+
+        ticketCard.innerHTML = `
+        <div class="card my-3">
+      <div class="row g-0">
+        <div class="col-md-3">
+          <img src="./img/logo-italo-nuovo-trasporto-viaggiatori-bianco.jpg" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-9">
+          <div class="card-body">
+            <h5 class="card-title">${userCompleteName}</h5>
+            <p class="card-text">Percorrerai ${km} Km</p>
+            <p class="card-text">${discountPrice}% di sconto per te</p>
+            <p class="card-text">Prezzo: ${ticketPrice}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+        `
+
+
+
+
+
+    }
 
 
 
